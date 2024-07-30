e0ce67cf3521f62e48910c388d6746d321a5a376
Ololade Otemade <adeniyiotemade@gmail.com>
2024-07-30 17:35:30 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,10 +7,10 @@ const header = {
 const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Ololade Otemade',
-  role: 'Software Engineer',
+  role: 'Frontend Engineer',
   description:
     'I am a detail-oriented software engineer dedicated to crafting user-friendly applications and delivering exceptional user experiences. Passionate about leveraging my expertise to ensure the highest standards of security in fintech development. Committed to excellence, I excel in creating quality applications that consistently exceed user expectations.',
-  resume: 'https://drive.google.com/file/d/1I-mD2TGvZRaPnFZkUgzw2Lt_tQyTWB2M/view?usp=drive_link',
+  resume: 'https://drive.google.com/file/d/15pMxgtQ0SEZ1oGPvXYgkX63RxbOsycWN/view?usp=drive_link',
   social: {
     linkedin: 'https://www.linkedin.com/in/ololade-otemade-72959613a/',
     github: 'https://github.com/iamLolade',
@@ -29,20 +29,20 @@ const projects = [
     livePreview: 'https://fluently-app.vercel.app',
   },
   {
-    name: 'Grocerease',
+    name: 'Wordana',
     description:
-      'Your virtual marketplace for all things groceries! Effortlessly browse products, explore categories, access services, and purchase your grocery. ',
+      'Wordana is a wordle game that is built on the Lightlink blockchain network. The game makes use of the API 3 Quantum Random Number Generator protocol to whip up a collection of random words.',
     stack: ['Tailwind CSS', 'Next', 'Typescript', 'Hooks'],
-    sourceCode: 'https://github.com/iamLolade/grocerease-store',
-    livePreview: 'https://grocerease-store.vercel.app',
+    sourceCode: 'https://github.com/Havorahq/wordana',
+    livePreview: 'https://wordana.vercel.app/',
   },
   {
-    name: 'Grocerease - Admin',
+    name: 'Xalari',
     description:
-      'Your central command for managing the grocery store! Effortlessly oversee products, analyze sales, access analytics, and stay connected for a streamlined administrative experience.',
+      'Blockchain payroll service for the new wave of remote work. You gain the ability to hire with flexibility from any part of the world and make seamless crypto payments to your employees & contractors',
     stack: ['Tailwind CSS', 'Next', 'Typescript', 'Hooks'],
-    sourceCode: 'https://github.com/iamLolade/grocerease',
-    livePreview: 'https://grocerease-admin.vercel.app',
+    sourceCode: 'https://github.com/Havorahq/payrol',
+    livePreview: 'https://app.xalari.com/',
   }
 ]
 
@@ -58,13 +58,17 @@ const skills = [
   'Redux',
   'SASS',
   'Material UI',
+  'Chakra UI',
+  'Radix UI',
   'Styled-Components',
   'Tailwind CSS',
+  'Shadcn',
   'Git',
   'CI/CD',
   'Jest',
   'MongoDB',
   'Node',
+  'Solidity'
 ]
 
 const contact = {
